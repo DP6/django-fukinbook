e10e213704543fb926ba3a2e098934accf3c5fe8
gabrielhpugliese <gabrielhpugliese@gmail.com>
2012-03-26 19:44:41 -0300

    Added some examples. FQL sample does not work yet.

--- django_fukinbook/graph_api.py
+++ django_fukinbook/graph_api.py
@@ -3,6 +3,7 @@ from session import FacebookSessionError
 import urlparse
 import urllib
 import simplejson
+import pprint
 
 # TODO: API_URL is duplicated with session module. Fix it.
 class GraphAPI:
@@ -12,16 +13,21 @@ class GraphAPI:
     def __init__(self, access_token=None):
         self.access_token = access_token
         
-    def get(self, path='me', connection_type=None, metadata=False):
+    def get(self, path='me', fql=None, connection_type=None, metadata=False, format='json'):
         url = '%s%s' % (self.API_URL, path)
         if connection_type:
             url += '/%s' % (connection_type)
 
-        params = {'access_token': self.access_token}
+        params = {'access_token': self.access_token,
+                  'format': format}
         if metadata:
             params['metadata'] = 1
+        if fql:
+            params['q'] = fql
 
         url += '?%s' % urllib.urlencode(params)
+        pp = pprint.PrettyPrinter()
+        pp.pprint(url)
         try:
             response = simplejson.load(urllib.urlopen(url))
         except Exception, e:
@@ -31,3 +37,19 @@ class GraphAPI:
             error = response['error']
             raise FacebookSessionError(error['type'], error['message'])
         return response
+
+    def get_significant_other(self, object_id='me'):
+        user = self.get(path=object_id)
+        if 'significant_other' in user:
+            significant_other = user['significant_other']
+            return significant_other
+        return None
+    
+    def get_upcoming_birthdates(self):
+        #fql = 'select name, uid, birthday_date from user where uid in (select uid2 from friend where uid1=me()) and strlen(birthday_date) != 0 and ((substr(birthday_date, 0, 2) = 03 and substr(birthday_date, 3, 5) > 26) or (substr(birthday_date, 0, 2) = 05 and substr(birthday_date, 3, 5) < 26)) order by birthday_date'
+        #fql = 'select name, uid, birthday_date from user where uid in (select uid2 from friend where uid1=me())'
+        fql = '''select name, uid, birthday_date from user where uid in (select uid2 from friend where uid1=me()) and strlen(birthday_date) != 0 and ((substr(birthday_date, 0, 2) = '03' and substr(birthday_date, 3, 5) > '26') or (substr(birthday_date, 0, 2) = '05' and substr(birthday_date, 3, 5) < '26')) order by birthday_date limit 5'''
+        response = self.get(path='fql', fql=fql)
+        return response
+        
+        
--- django_fukinbook/views.py
+++ django_fukinbook/views.py
@@ -4,16 +4,23 @@ from django.shortcuts import render_to_response, HttpResponse
 from django.template import Context
 from django.views.decorators.csrf import csrf_exempt
 
+import pprint
+
 import settings
 from models import Token
 from session import FacebookSession
+from graph_api import GraphAPI
 
 def canvas(request):
     try:
         token = Token.objects.get(user=request.user)
     except:
         HttpResponseRedirect('/404/')
-    return HttpResponse(token)
+    api = GraphAPI(token.access_token)
+    a = api.get_upcoming_birthdates()
+    pp = pprint.PrettyPrinter()
+    pp.pprint(a)
+    return HttpResponse(a['data'])
 
 @csrf_exempt
 def login(request):
